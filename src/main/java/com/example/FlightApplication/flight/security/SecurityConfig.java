2e5fb10d95a851a71a70e1bd9e117cba918aa1d9
Abhishek Pawar <98691564+abhipawar0312@users.noreply.github.com>
2025-05-28 23:22:50 +0530

    created WebService for Registration

---
+++ src/main/java/com/example/FlightApplication/flight/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.FlightApplication.flight.security;
+
+import org.springframework.context.annotation.Bean;
+
+public class SecurityConfig {
+    //security configuration code goes here
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**", "/register/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+
+}
