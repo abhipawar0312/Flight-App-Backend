a1424aa25c5433c333c175e1f553c2402d928e49
Abhishek Pawar <98691564+abhipawar0312@users.noreply.github.com>
2025-06-03 19:49:36 +0530

    created

--- src/main/java/com/example/FlightApplication/flight/security/JwtTokenFilter.java
+++ src/main/java/com/example/FlightApplication/flight/security/JwtTokenFilter.java
@@ -1,6 +1,13 @@
 package com.example.FlightApplication.flight.security;
 
+import java.security.Key;
+
 public class JwtTokenFilter {
+
+
+
+
+
 
 
 
---
+++ src/main/java/com/example/FlightApplication/flight/security/JwtUtil.java
@@ -0,0 +1,13 @@
+package com.example.FlightApplication.flight.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "ThisIsAVeryLongSecretKeyForJwtThatHasAtLeastSixtyFourCharacters12345";
+
+    public String genarateToken(String username){
+        return null;
+
+    }
+}
